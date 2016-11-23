4bfc08dd991ffc03f3a56ce4cc4ab4e785cbf8c9
Aby Herwendo <AbyHer@corp.vico.co.id>
2016-11-23 11:43:36 +0700

    fix elastic index

--- tarikh/search_indexes.py
+++ tarikh/search_indexes.py
@@ -6,7 +6,7 @@ from haystack import indexes
 from tarikh.models import Topic, Event
 
 class TopicIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.EdgeNgramField(document=True, use_template=True)
+    text = indexes.NgramField(document=True, use_template=True)
     pub_date = indexes.DateTimeField(model_attr='published_date')
     
     # We add this for autocomplete.
